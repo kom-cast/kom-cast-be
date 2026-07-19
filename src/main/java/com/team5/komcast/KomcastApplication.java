6108daa21d9bec8a8081e051a3af7d91bbdbd3d0
5ms <oms010414@gmail.com>
2026-07-19 20:24:58 +0900

    chore: setup Spring project structure

---
+++ src/main/java/com/team5/komcast/KomcastApplication.java
@@ -0,0 +1,13 @@
+package com.team5.komcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomcastApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/team5/komcast/testController/TestController.java
@@ -0,0 +1,12 @@
+package com.team5.komcast.testController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
