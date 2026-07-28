d93a489541fea403e1a24a593a0306c64175dada
Yunju07 <juile0827@naver.com>
2026-07-28 13:28:24 +0900

    feat(#16): configure relative Server path in SwaggerConfig for production deployment host

--- src/main/java/com/komcast/be/config/SwaggerConfig.java
+++ src/main/java/com/komcast/be/config/SwaggerConfig.java
@@ -5,11 +5,19 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.server-url:}")
+    private String customServerUrl;
 
     @Bean
     public OpenAPI openAPI() {
@@ -24,11 +32,21 @@ public class SwaggerConfig {
         SecurityRequirement securityRequirement = new SecurityRequirement()
                 .addList(securityHeaderName);
 
+        List<Server> servers = new ArrayList<>();
+
+        if (customServerUrl != null && !customServerUrl.isBlank()) {
+            servers.add(new Server().url(customServerUrl).description("Production Deployment Server"));
+        }
+
+        // Relative URL server automatically points Swagger UI to the current hosting server IP/domain
+        servers.add(new Server().url("/").description("Current Host Server"));
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Kom-Cast REST API Specification")
                         .description("Kom-Cast 개인화 AI 브리핑 서비스를 위한 백엔드 API 명세서입니다.")
                         .version("v1.0.0"))
+                .servers(servers)
                 .addSecurityItem(securityRequirement)
                 .components(new Components()
                         .addSecuritySchemes(securityHeaderName, securityScheme));
