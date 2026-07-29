c943c64a59075cf2f56f857985dc85114ee40a17
Yunju07 <juile0827@naver.com>
2026-07-29 11:43:29 +0900

    fix: make updated_at nullable in BaseTimeEntity

--- src/main/java/com/komcast/be/domain/BaseTimeEntity.java
+++ src/main/java/com/komcast/be/domain/BaseTimeEntity.java
@@ -20,6 +20,6 @@ public abstract class BaseTimeEntity {
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(name = "updated_at", nullable = false)
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
